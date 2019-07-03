7b62d44e38309c5694cd2f4baaacefd885791b79
L2zz <ulla4571@gmail.com>
2019-07-04 00:30:21 +0900

    Use Butterknife to binding

--- app/src/main/java/edu/skku/jonadan/hangangmongttang/MapActivity.java
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/MapActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import net.daum.mf.map.api.MapView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MapActivity extends AppCompatActivity {
 
-    private MapView mapView;
+    @BindView(R.id.map_view)
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
-        mapView = findViewById(R.id.map_view);
+        ButterKnife.bind(this);
     }
 }
