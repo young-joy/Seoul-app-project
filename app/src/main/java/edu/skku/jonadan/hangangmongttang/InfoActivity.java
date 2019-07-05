5ed68f47dab67d99593c21712b616339ee1c3635
Gongmeda_desk <dudals1303@naver.com>
2019-07-06 01:25:59 +0900

    simple UI

---
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/InfoActivity.java
@@ -0,0 +1,14 @@
+package edu.skku.jonadan.hangangmongttang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
