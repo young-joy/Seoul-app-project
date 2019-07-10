fdcb33b8e8a7ae597d1644125efe246551cca869
Gongmeda_desk <gongmeda@gmail.com>
2019-07-11 01:17:17 +0900

    top bar back button

--- app/src/main/java/edu/skku/jonadan/hangangmongttang/InfoActivity.java
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/InfoActivity.java
@@ -3,12 +3,27 @@ package edu.skku.jonadan.hangangmongttang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class InfoActivity extends AppCompatActivity {
+
+    @BindView(R.id.info_back_btn) ImageButton backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        ButterKnife.bind(this);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
