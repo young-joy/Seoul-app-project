45887d052af407563420b6b28240564c8203eccb
guswh11 <38255974+guswh11@users.noreply.github.com>
2019-08-18 22:43:57 +0900

    Update ParkInfoCrawler.java
    implement get_weather func.

--- app/src/main/java/edu/skku/jonadan/hangangmongttang/ParkInfoCrawler.java
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/ParkInfoCrawler.java
@@ -13,17 +13,21 @@ import java.util.HashMap;
 
 public class ParkInfoCrawler {
     private static String htmlUrl = "https://hangang.seoul.go.kr/";
-    private static ArrayList<HashMap<String, String>> eventList = null;
-
+    private static ArrayList<HashMap<String, String>> eventList = new ArrayList<>();
+    private static HashMap<String, String> weatherInfo = new HashMap<>();
     private static int events_cnt = 0;
 
-    public static void start(){
-        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
-        jsoupAsyncTask.execute();
+    public static HashMap<String, String> getWeatherInfo() {
+        return weatherInfo;
     }
 
     public static ArrayList<HashMap<String, String>> getEventList(){
         return eventList;
+    }
+
+    public static void start(){
+        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+        jsoupAsyncTask.execute();
     }
 
     private static class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
@@ -33,6 +37,8 @@ public class ParkInfoCrawler {
             try {
                 events_cnt = 0;
                 Document doc = Jsoup.connect(htmlUrl).get();
+
+                //get event_info
                 Elements events = doc.select("div.left h5");
                 Elements events_place = doc.select("div.left span.place");
                 Elements events_date = doc.select("p.right span.date");
@@ -40,17 +46,30 @@ public class ParkInfoCrawler {
 
                 for(Element e: events){
                     HashMap<String, String> eventInfo = new HashMap<>();
-                    Log.d("jsoup_parser", e.text());
-                    Log.d("jsoup_parser", events_date.get(events_cnt).text());
-
-                    eventInfo.put("NAME",e.text().trim());
-                    eventInfo.put("PLACE",events_place.get(events_cnt).text().trim());
+                    eventInfo.put("NAME", e.text().trim());
+                    eventInfo.put("PLACE", events_place.get(events_cnt).text().trim());
                     eventInfo.put("DATE", events_date.get(events_cnt).text().trim());
                     eventInfo.put("TIME", events_time.get(events_cnt).text().trim());
 
+                    Log.d("jsoup_parser",eventInfo.get("NAME"));
                     eventList.add(eventInfo);
                     events_cnt++;
                 }
+
+                //get weather_info
+                Element weather_today = doc.selectFirst("div.weather span");
+                Element weather_temp = doc.selectFirst("div.weather p.weather_txt");
+                Elements weather_dust = doc.select("div.weather span.air_grade");
+
+                weatherInfo.put("TODAY", weather_today.text().trim());
+                weatherInfo.put("TEMP", weather_temp.text().trim());
+                weatherInfo.put("DUST-G1", weather_dust.get(0).text().trim());
+                weatherInfo.put("DUST-G2", weather_dust.get(1).text().trim());
+
+                Log.d("jsoup_parser_weather",weatherInfo.get("TODAY"));
+                Log.d("jsoup_parser_weather",weatherInfo.get("TEMP"));
+                Log.d("jsoup_parser_weather",weatherInfo.get("DUST-G1"));
+                Log.d("jsoup_parser_weather",weatherInfo.get("DUST-G2"));
             }catch (Exception e){
                 e.printStackTrace();
             }
