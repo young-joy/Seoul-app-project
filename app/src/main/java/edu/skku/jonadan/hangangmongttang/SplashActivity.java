bc3c8fd1b26265c103b9251bb9aa3c80b411c597
Gongmeda <gongmeda@gmail.com>
2019-08-22 15:55:32 +0900

    Change splash screen (logo and animation)

--- app/src/main/java/edu/skku/jonadan/hangangmongttang/SplashActivity.java
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/SplashActivity.java
@@ -1,23 +1,36 @@
 package edu.skku.jonadan.hangangmongttang;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class SplashActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
     public static Context mContext;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        logo = (ImageView)findViewById(R.id.app_logo);
+        mContext = this;
 
-        mContext= this;
+        // Fullscreen (no top bar)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        ParkInfoCrawler.start();    // start loading
+        // Logo Animation
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.splash_anim);
+        logo.startAnimation(anim);
+
+        // Start Loading Info
+        ParkInfoCrawler.start();
     }
 
     public void loaded() {
