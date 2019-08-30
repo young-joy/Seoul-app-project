32f71e7db8dfdfc1086dd79a06d7d69ca401e0f0
Hyunjin <guswh11@naver.com>
2019-08-30 20:07:45 +0900

    Implement review_list

--- app/src/main/java/edu/skku/jonadan/hangangmongttang/ReviewFragment.java
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/ReviewFragment.java
@@ -5,16 +5,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class ReviewFragment extends Fragment {
+    ListView reviewListView;
+
+    private ArrayList<ReviewListItem> reviewList = new ArrayList<>();
+    private ReviewListAdapter reviewListAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_review, container, false);
+
+        reviewListView = rootView.findViewById(R.id.review_list);
+
+        for(int i=0;i<3;i++){
+            ReviewListItem item = new ReviewListItem();
+            reviewList.add(item);
+        }
+        reviewListAdapter = new ReviewListAdapter(reviewList);
+        reviewListView.setAdapter(reviewListAdapter);
 
         return rootView;
     }
---
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/ReviewListAdapter.java
@@ -0,0 +1,44 @@
+package edu.skku.jonadan.hangangmongttang;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class ReviewListAdapter extends BaseAdapter {
+    private ArrayList<ReviewListItem> reviewList = new ArrayList<>();
+
+    public ReviewListAdapter(ArrayList<ReviewListItem> reviewList) {
+        this.reviewList = reviewList;
+    }
+
+    @Override
+    public int getCount() {
+        return reviewList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return reviewList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        final Context context = viewGroup.getContext();
+
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.item_review, viewGroup, false);
+        }
+
+        return view;
+    }
+}
