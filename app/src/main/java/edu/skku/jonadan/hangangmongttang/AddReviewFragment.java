a7427d192c65f2085cbcbd882019dec9d73ed467
Hyunjin <guswh11@naver.com>
2019-09-05 21:28:04 +0900

    Create add_review dialogFragment

---
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/AddReviewFragment.java
@@ -0,0 +1,24 @@
+package edu.skku.jonadan.hangangmongttang;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class AddReviewFragment extends DialogFragment {
+    public AddReviewFragment() {
+        super();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_review, container, false);
+
+        return view;
+    }
+}
