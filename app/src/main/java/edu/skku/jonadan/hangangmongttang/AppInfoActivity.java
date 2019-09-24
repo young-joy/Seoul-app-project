79b2267bc92146237ac54ecebed7d792fb5cb758
Gongmeda <gongmeda@gmail.com>
2019-09-25 00:37:37 +0900

    Implement open source license activity

--- app/src/main/java/edu/skku/jonadan/hangangmongttang/AppInfoActivity.java
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/AppInfoActivity.java
@@ -2,17 +2,22 @@ package edu.skku.jonadan.hangangmongttang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
+
 public class AppInfoActivity extends AppCompatActivity {
     @BindView(R.id.appinfo_back_btn) ImageButton backBtn;
     @BindView(R.id.appinfo_icon) ImageView iconView;
+    @BindView(R.id.appinfo_btn3) Button osBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,14 @@ public class AppInfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        osBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AppInfoActivity.this, OssLicensesMenuActivity.class);
+                startActivity(intent);
             }
         });
     }
