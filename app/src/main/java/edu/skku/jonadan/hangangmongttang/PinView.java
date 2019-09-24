b9f4cc8ce46db089b1fa453c2557723dab8a438c
Hyunjin <guswh11@naver.com>
2019-09-24 13:41:29 +0900

    Update PinView.java
    implement pin_touched event

--- app/src/main/java/edu/skku/jonadan/hangangmongttang/PinView.java
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/PinView.java
@@ -27,8 +27,12 @@ public class PinView extends SubsamplingScaleImageView {
     ArrayList<MapPin> mapPins;
     ArrayList<DrawPin> drawnPins;
     ArrayList<PointF> deeplinkCoordinates = new ArrayList<>();
+    ArrayList<PointF> modifiedCoordinates = new ArrayList<>();
     Context context;
     String tag = getClass().getSimpleName();
+
+    float pin_width;
+    float pin_height;
 
     public PinView(Context context) {
         this(context, null);
@@ -82,7 +86,11 @@ public class PinView extends SubsamplingScaleImageView {
         display.getSize(sizePoint);
         float width = sizePoint.x;
         float height = sizePoint.y;
+        pin_width = (density / 160f) * 30f;
+        pin_height = (density / 160f) * 30f;
         Log.d("get_size",sizePoint.toString());
+
+        modifiedCoordinates.clear();
 
         for (int i = 0; i < mapPins.size(); i++) {
             MapPin mPin = mapPins.get(i);
@@ -90,30 +98,28 @@ public class PinView extends SubsamplingScaleImageView {
             //Bitmap bmpPin = Utils.getBitmapFromAsset(context, mPin.getPinImgSrc());
             Bitmap bmpPin = MainActivity.getBitmapFromVectorDrawable(context, R.drawable.marker);
 
-            float w = (density / 160f) * 30;
-            float h = (density / 160f) * 30;
-            bmpPin = Bitmap.createScaledBitmap(bmpPin, (int) w, (int) h, true);
+            bmpPin = Bitmap.createScaledBitmap(bmpPin, (int) pin_width, (int) pin_height, true);
 
             //PointF vPin = sourceToViewCoord(mPin.getPoint());
             PointF vPin = mPin.getPoint();
             //in my case value of point are at center point of pin image, so we need to adjust it here
 
-            //Log.d("touch_event",vPin.toString());
-            Log.d("touch_event","density : "+new Float(density).toString());
-            float x_px = (640f/160f)*(vPin.x - (bmpPin.getWidth() / 2));
-            float y_px = (640f/160f)*(vPin.y - bmpPin.getHeight()/2 -40);
-            Log.d("touch_event","px: "+new Float(x_px).toString() + ", "+ new Float(y_px).toString());
+            Log.d("touch_event",vPin.toString());
 
+            PointF newPin = new PointF();
             float vX = (vPin.x - (bmpPin.getWidth() / 2)) * width / 1440f;
             float vY = (vPin.y - bmpPin.getHeight()/2 -40f) * height / 2560f;
+            newPin.x = vX;
+            newPin.y = vY;
+            modifiedCoordinates.add(newPin);
             Log.d("touch_event","dp: "+new Float(vX).toString()+", "+new Float(vY).toString());
             canvas.drawBitmap(bmpPin, vX, vY, paint);
             //add added pin to an Array list to get touched pin
             DrawPin dPin = new DrawPin();
-            dPin.setStartX(mPin.getX() - w / 2);
-            dPin.setEndX(mPin.getX() + w / 2);
-            dPin.setStartY(mPin.getY() - h / 2);
-            dPin.setEndY(mPin.getY() + h / 2);
+            dPin.setStartX(newPin.x - pin_width/2);
+            dPin.setEndX(newPin.x + pin_width/2);
+            dPin.setStartY(newPin.y + pin_height/2);
+            dPin.setEndY(newPin.y + pin_height/ 2);
             dPin.setId(mPin.getId());
             drawnPins.add(dPin);
         }
@@ -137,22 +143,21 @@ public class PinView extends SubsamplingScaleImageView {
         final GestureDetector gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onSingleTapConfirmed(MotionEvent e) {
-                if (isReady() && deeplinkCoordinates != null) {
+                if (isReady() && modifiedCoordinates != null) {
                     PointF tappedCoordinate = new PointF(e.getX(), e.getY());
                     Log.d("touch_event", tappedCoordinate.toString());
-                    int blockWidth = 30;
-                    int blockHeight = 30;
 
-                    // check if deepLinkBitmap is touched
-                    for (PointF deeplink : deeplinkCoordinates) {
-                        PointF deeplinkCoordinate = sourceToViewCoord(deeplink);
-                        int deeplinkX = (int) (deeplinkCoordinate.x - (blockWidth / 2));
-                        int deeplinkY = (int) (deeplinkCoordinate.y - blockHeight);
+                    int pin_num = modifiedCoordinates.size();
+                    for (int i=0;i<pin_num;i++) {
+                        PointF pinCoord = modifiedCoordinates.get(i);
+                        int pinX = (int) pinCoord.x;
+                        int pinY = (int) pinCoord.y;
 
-                        // center coordinate -/+ blockWidth actually sets touchable area to 2x icon size
-                        if (tappedCoordinate.x >= deeplinkX - blockWidth && tappedCoordinate.x <= deeplinkX + blockWidth &&
-                                tappedCoordinate.y >= deeplinkY - blockHeight && tappedCoordinate.y <= deeplinkY + blockHeight) {
-                            Log.d("touch_event","pin touched");
+                        if (tappedCoordinate.x >= pinX - pin_width && tappedCoordinate.x <= pinX + pin_width &&
+                                tappedCoordinate.y >= pinY - pin_height && tappedCoordinate.y <= pinY + pin_height) {
+                            Log.d("touch_event",new Integer(i).toString() + "pin touched");
+                            Toast.makeText(context,"pin touched",Toast.LENGTH_SHORT).show();
+                            break;
                         }
                     }
                 }
