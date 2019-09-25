95a89e8d7255fed904edb7cc0963ae0664af6800
Gongmeda <gongmeda@gmail.com>
2019-09-25 20:24:41 +0900

    temporary commit

---
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/AppInfoItem.java
@@ -0,0 +1,17 @@
+package edu.skku.jonadan.hangangmongttang;
+
+public class AppInfoItem {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public AppInfoItem(String title) {
+        this.title = title;
+    }
+}
