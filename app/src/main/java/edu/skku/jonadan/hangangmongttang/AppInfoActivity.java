9853f6732b0e8f8ba85a22fa7177f943687a6d1a
Gongmeda <gongmeda@gmail.com>
2019-09-27 22:11:08 +0900

    For merging

--- app/src/main/java/edu/skku/jonadan/hangangmongttang/AppInfoActivity.java
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/AppInfoActivity.java
@@ -2,8 +2,10 @@ package edu.skku.jonadan.hangangmongttang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -11,8 +13,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AppInfoActivity extends AppCompatActivity {
+
     @BindView(R.id.appinfo_back_btn) ImageButton backBtn;
     @BindView(R.id.appinfo_icon) ImageView iconView;
+    @BindView(R.id.appinfo_btn1) Button noticeBtn;
+    @BindView(R.id.appinfo_btn3) Button osBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,22 @@ public class AppInfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        noticeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AppInfoActivity.this, AppNoticeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        osBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(AppInfoActivity.this, );
+                //startActivity(intent);
             }
         });
     }
