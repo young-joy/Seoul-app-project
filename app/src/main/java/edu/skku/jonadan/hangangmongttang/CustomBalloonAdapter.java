67acac64ba77c47875aa56dce3ab4b96ff73a8ac
L2zz <ulla4571@gmail.com>
2019-09-28 00:29:52 +0900

    Fix bug with balloon
    Keep visibility of balloon
    
    Signed-off-by: L2zz <ulla4571@gmail.com>

--- app/src/main/java/edu/skku/jonadan/hangangmongttang/CustomBalloonAdapter.java
+++ app/src/main/java/edu/skku/jonadan/hangangmongttang/CustomBalloonAdapter.java
@@ -86,6 +86,66 @@ public class CustomBalloonAdapter implements CalloutBalloonAdapter {
 
     @Override
     public View getPressedCalloutBalloon(MapPOIItem mapPOIItem) {
-        return null;
+        Log.d("Balloon", "" + mapPOIItem.getTag());
+        int type = mapPOIItem.getTag() / SeoulApiProvider.SERVICE_PAD;
+        Log.d("Balloon", "" + type);
+        switch (SeoulApiProvider.SERVICE_CODE.values()[type]) {
+            case USER:
+                mBalloon = LayoutInflater.from(context).inflate(R.layout.custom_balloon, null);
+                mBalloon.setVisibility(View.VISIBLE);
+
+                LinearLayout bgUser = mBalloon.findViewById(R.id.balloon_bg);
+                bgUser.setBackgroundResource(R.drawable.ic_balloon_user);
+
+                TextView titleUser = mBalloon.findViewById(R.id.balloon_title);
+                titleUser.setText(mapPOIItem.getItemName());
+
+                mBalloon.findViewById(R.id.balloon_enter).setVisibility(View.GONE);
+                break;
+            case PARK:
+                if (mapPOIItem.getTag() % SeoulApiProvider.SERVICE_PAD % 1000 != 0) {
+                    mBalloon = LayoutInflater.from(context).inflate(R.layout.custom_balloon_park, null);
+                    mBalloon.setVisibility(View.VISIBLE);
+
+                    TextView title = mBalloon.findViewById(R.id.balloon_park_title);
+                    title.setText(mapPOIItem.getItemName());
+
+                    TextView state = mBalloon.findViewById(R.id.balloon_park_state);
+                    int parkId = mapPOIItem.getTag() % SeoulApiProvider.SERVICE_PAD / 1000;
+                    int parkingLotId = mapPOIItem.getTag() % SeoulApiProvider.SERVICE_PAD % 1000 - 1;
+                    String capacity = new Integer(ConstantPark.PARK_LIST.get(parkId)
+                            .getParkingLots().get(parkingLotId)
+                            .getCapacity()).toString();
+                    String curState = "0";
+                    state.setText(curState + " / " + capacity);
+                } else {
+                    mBalloon = LayoutInflater.from(context).inflate(R.layout.custom_balloon, null);
+                    mBalloon.setVisibility(View.VISIBLE);
+
+                    LinearLayout bg = mBalloon.findViewById(R.id.balloon_bg);
+                    bg.setBackgroundResource(R.drawable.ic_balloon_park);
+
+                    TextView title = mBalloon.findViewById(R.id.balloon_title);
+                    title.setText(mapPOIItem.getItemName());
+
+                    mBalloon.findViewById(R.id.balloon_enter).setVisibility(View.GONE);
+                }
+                break;
+            default:
+                mBalloon = LayoutInflater.from(context).inflate(R.layout.custom_balloon, null);
+                mBalloon.setVisibility(View.VISIBLE);
+
+                TextView title = mBalloon.findViewById(R.id.balloon_title);
+                if (mapPOIItem.getItemName().equals("")) {
+                    mBalloon.setVisibility(View.INVISIBLE);
+                } else {
+                    title.setText(mapPOIItem.getItemName());
+                    LinearLayout bg = mBalloon.findViewById(R.id.balloon_bg);
+                    bg.setBackgroundResource(R.drawable.ic_balloon);
+                    mBalloon.findViewById(R.id.balloon_enter).setVisibility(View.VISIBLE);
+                }
+        }
+
+        return mBalloon;
     }
 }
