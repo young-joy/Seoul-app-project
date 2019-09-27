1b9d3f19e9dad530907ed74827e54bd2fd6d06d2
L2zz <ulla4571@gmail.com>
2019-09-27 16:42:41 +0000

    Connect db and crawling code
    Signed-off-by: L2zz <ulla4571@gmail.com>

--- crawling.py
+++ crawling.py
@@ -1,5 +1,15 @@
+import pymysql
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+
+db = pymysql.connect(host="localhost",
+                     user="seoul",
+                     passwd="qwer1234",
+                     db="seoul_database",
+                     charset="utf8")
+
+cur = db.cursor()
 
 url = "https://hangang.seoul.go.kr/"
 html = urlopen(url)
@@ -10,12 +20,18 @@ soup = BeautifulSoup(source, "html.parser")
 event_table = soup.find(class_="event_list")
 event_list = event_table.find_all("li")
 
+cur.execute("TRUNCATE event")
 for event in event_list:
     place = event.find(class_="place")
     title = event.find("h5")
     date = event.find(class_="date")
     time = event.find(class_="time")
-    print(place.get_text())
-    print(title.get_text())
-    print(date.get_text())
-    print(time.get_text())
+    query = ("INSERT INTO event (title, location, date, time)" 
+              + " VALUES ('" 
+              + title.string + "', '" 
+              + place.string + "', '" 
+              + date.string + "', '" 
+              + time.string + "')")
+    cur.execute(query)
+db.commit()
+db.close()
